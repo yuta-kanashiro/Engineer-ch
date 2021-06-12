81d3dd6e9bf6b60cdcf0292ae2c55bf9d7f61562
yutakanashiro <kanashiro713@gmail.com>
2021-06-12 18:22:49 +0900

    ユーザー詳細画面のview微調整

--- src/resources/views/users/show.blade.php
+++ src/resources/views/users/show.blade.php
@@ -16,11 +16,13 @@
                     <div class="col-lg-8 ml-2">
                         <div class="d-flex">
                             <h5 class="my-3 text-dark font-weight-bold">{{ $user->name }}</h5>
-                            @if (Auth::id() == $user->id)
-                                <a href="{{ route('user.edit', Auth::user()) }}" class="btn orange-color text-white ml-auto"> 編集</a>
-                            @else
-                                <button class="btn-sm orange-color text-white ml-auto"> フォロー</button>
-                            @endif
+                            @auth
+                                @if (Auth::id() == $user->id)
+                                    <a href="{{ route('user.edit', Auth::user()) }}" class="btn orange-color text-white ml-auto"> 編集</a>
+                                @else
+                                    <button class="btn-sm orange-color text-white ml-auto"> フォロー</button>
+                                @endif
+                            @endauth
                         </div>
                         <div class="d-flex flex-row mt-3">
                             <p class="mr-3 text-muted">年齢：{{ $user->age }}歳</p>
@@ -54,7 +56,7 @@
                                         @else
                                             <img class="profile-icon rounded-circle" src="{{ Storage::url($user->profile_image) }}" alt="プロフィール画像" width="30" height="30">
                                         @endif
-                                        <small class="mt-1 ml-2 text-muted"><a href="{{ route('login') }}" class="text-dark">{{ $user->name }}</a>が{{ $bulletin->created_at->format('Y年m月d日') }}に投稿</small>
+                                        <small class="mt-1 ml-2 text-muted"><a href="#" class="text-dark">{{ $user->name }}</a>が{{ $bulletin->created_at->format('Y年m月d日') }}に投稿</small>
                                     </div>
                                     <a href="" class="card-body">
                                         <div class="row">

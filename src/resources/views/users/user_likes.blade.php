99ebb7ef6eea00737b2cc8862503c26a50fcc012
yutakanashiro <kanashiro713@gmail.com>
2022-03-31 17:31:30 +0900

    微修正

--- src/resources/views/users/user_likes.blade.php
+++ src/resources/views/users/user_likes.blade.php
@@ -15,6 +15,7 @@
                 </div>
                 <a href="{{ route('bulletin.show', $like_bulletin) }}">
                     <h5 class="text-dark font-weight-bold">{{ $like_bulletin->title }}</h5>
+                    <small class="text-muted">いいね数 {{ $like_bulletin->countLikes() }}</small>
                     <small class="text-muted">コメント数 {{ $like_bulletin->countComments() }}</small>
                 </a>
             </div>

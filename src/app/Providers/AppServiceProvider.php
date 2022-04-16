44a2f35f36fc3dbaa90fcc330354b8b5ed88dba2
yutakanashiro <kanashiro713@gmail.com>
2022-04-16 18:56:19 +0900

    強制https化

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }

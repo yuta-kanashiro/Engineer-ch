fef66a55cc7f45bf936bf35422b9ee0b4ac847f1
yutakanashiro <kanashiro713@gmail.com>
2022-04-17 18:44:47 +0900

    強制https化

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
         // 本番環境(Heroku)でhttpsを強制する
         if (\App::environment('production')) {
             \URL::forceScheme('https');
+        }
+
+        if(env('FORCE_HTTPS',false)) {
+            URL::forceScheme('https');
         }
     }
 }
